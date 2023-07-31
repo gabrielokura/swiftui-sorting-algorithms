1b3e392b14375f4dc15edad65ea3c2296b0dafe2
Gabriel Okura <gabriellevicz@gmail.com>
2023-07-31 18:44:49 -0300

    Initial Commit

---
+++ QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by Gabriel Motelevicz Okura on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
